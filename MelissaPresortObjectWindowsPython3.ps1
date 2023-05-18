47e701c521c874718bad7c23cf522136ae2ff065
Melissa <>
2023-05-18 08:52:53 -0700

    Tested successfully using Melissa data files for 2023.04

--- MelissaPresortObjectWindowsPython3.ps1
+++ MelissaPresortObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class DLLConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.03'
+$RELEASE_VERSION = '2023.04'
 $ProductName = "presort_data"
 
 # Uses the location of the .ps1 file 
