15f6671854949f26def7227cfde7d77f7457a56b
Melissa <ha@melissadata.com>
2024-02-23 14:51:18 -0800

    Tested successfully using Melissa data files for 2024.01

--- MelissaPresortObjectWindowsPython3.ps1
+++ MelissaPresortObjectWindowsPython3.ps1
@@ -19,7 +19,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2023.11'
+$RELEASE_VERSION = '2024.01'
 $ProductName = "presort_data"
 
 # Uses the location of the .ps1 file 
